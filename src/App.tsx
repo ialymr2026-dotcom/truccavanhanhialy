77de6af0f8b798af9a0ea33d96a81dbd79fbf9c0
ialymr2026-dotcom <ialymr2026@gmail.com>
2026-04-06 09:14:59 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -725,13 +725,15 @@ export default function App() {
       </div>
 
       <div className="card">
-        <div className="ctitle">Thông tin nghỉ phép</div>
+        <div className="ctitle">Thông tin nghỉ phép 
         <button
   className="btn-ex btn-word"
   onClick={() => window.open("https://script.google.com/macros/s/AKfycbwo8YVh0YbMLg3KSMoULVRG3moktSEodmY-H3ppk1ZJ0iia6hKxC-xkCKi6-WtKlBpG/exec", "_blank")}
 >
   Bảng báo cơm ca
 </button>
+        </div>
+        
         {alert && <div className={`alert ${alert.startsWith('✅') ? 'asuc' : 'aerr'}`}>{alert}</div>}
         <div className="g2">
           <div className="field">
